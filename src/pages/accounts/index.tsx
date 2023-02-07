01d08fdd7f72de363b19605fda334675fb9032a6
KEHINDE WAKEEL <kehinde.wakeel@indicina.co>
2023-02-07 16:50:22 +0100

    account screen updated and refactoring done

--- src/pages/accounts/index.tsx
+++ src/pages/accounts/index.tsx
@@ -1,6 +1,6 @@
 import PageSubHeader from '@/components/accounts/PageSubHeader';
 import Layout from '@/HOC/Layout';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import {
   DataTable,
   Table,
@@ -9,16 +9,110 @@ import {
   TableHeader,
   TableBody,
   TableCell,
+  Checkbox,
 } from '@carbon/react';
 import { isEmpty } from 'lodash';
 import Empty from '@/components/shared/Empty';
-import { headers, rolesheader, rolesRows, rows } from '@/utils/dtos';
+import { roles, users } from '@/mockData/index';
+import ActionIcons from '@/components/accounts/ActionIcons';
+import { AccessStatus } from '@/components/accounts/AccessStatus';
 
 const Accounts = () => {
   const data: any[] = [];
   const [selected, setSelected] = useState(0);
-  const [Headers, setHeaders] = useState<any[]>(headers);
-  const [Rows, setRows] = useState<any[]>(rows);
+  const [Headers, setHeaders] = useState<any[]>([]);
+  const [Rows, setRows] = useState<any[]>([]);
+
+  const usersheader = useMemo(() => {
+    return [
+      {
+        key: 'item_key',
+        header: <Checkbox id="checked-1" labelText="" />,
+      },
+      {
+        key: 'first_name',
+        header: 'First Name',
+      },
+      {
+        key: 'last_name',
+        header: 'lastName',
+      },
+      {
+        key: 'email',
+        header: 'Email Address',
+      },
+      {
+        key: 'access_status',
+        header: 'Access Status',
+      },
+      {
+        key: 'role',
+        header: 'Role',
+      },
+      {
+        key: 'authentication_type',
+        header: 'Authentication Type',
+      },
+      {
+        key: 'others',
+        header: '',
+      },
+    ];
+  }, []);
+  
+  const rolesheader = useMemo(() => {
+    return [
+      {
+        key: 'item_key',
+        header: <Checkbox id="checked-1" labelText="" />,
+      },
+      {
+        key: 'role_name',
+        header: 'Role Name',
+      },
+      {
+        key: 'description',
+        header: 'Description',
+      },
+      {
+        key: 'number',
+        header: 'Number',
+      },
+      {
+        key: 'others',
+        header: '',
+      },
+    ];
+  }, []);
+
+  const roleData = useMemo(() => {
+    return roles.map((role, _) => {
+      return {
+        id: role.id,
+        item_key: <Checkbox id="checked-2" labelText="" />,
+        role_name: role.role_name,
+        description: role.description,
+        number: role.number,
+        others: <ActionIcons />,
+      };
+    });
+  }, []);
+
+  const userData = useMemo(() => {
+    return users.map((user, _) => {
+      return {
+        id: user.id,
+        item_key: <Checkbox id="checked-2" labelText="" />,
+        first_name: user.first_name,
+        last_name: user.last_name,
+        email: user.email,
+        access_status: <AccessStatus active={user.access_status} />,
+        role: user.role,
+        authentication_type: user.authentication_type,
+        others: <ActionIcons />,
+      };
+    });
+  }, []);
 
   const handleSetIndex = (index: number) => {
     setSelected(index);
@@ -27,12 +121,12 @@ const Accounts = () => {
   useEffect(() => {
     if (selected === 1) {
       setHeaders(rolesheader);
-      setRows(rolesRows);
+      setRows(roleData);
     } else {
-      setHeaders(headers);
-      setRows(rows);
+      setHeaders(usersheader);
+      setRows(userData);
     }
-  }, [selected]);
+  }, [roleData, rolesheader, selected, userData, usersheader]);
 
   return (
     <Layout
