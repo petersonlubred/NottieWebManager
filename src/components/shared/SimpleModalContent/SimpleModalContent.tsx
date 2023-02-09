1226d03e175a4c05bbaa15defd14b08b6907bb00
KEHINDE WAKEEL <kehinde.wakeel@indicina.co>
2023-02-09 17:58:39 +0100

    profile subscription bulk upload added

--- src/components/shared/SimpleModalContent/SimpleModalContent.tsx
+++ src/components/shared/SimpleModalContent/SimpleModalContent.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 interface Iprops {
-  content: string;
+  content: string | JSX.Element;
 }
 const SimpleModalcontent = ({ content }: Iprops) => {
   return <Paragraph>{content}</Paragraph>;
