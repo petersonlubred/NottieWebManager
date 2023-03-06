b1175b822ada1781190b2a38d9b6687e24c001dc
KEHINDE WAKEEL <kehinde.wakeel@indicina.co>
2023-03-06 08:11:10 +0100

    chore: consumed system config endpoints

---
+++ src/redux/api/systemConfigApi.ts
@@ -0,0 +1,21 @@
+import { BaseQueryFn, createApi, FetchArgs } from '@reduxjs/toolkit/query/react';
+
+import { ConfigMenuResponse, ConfigsResponse } from './../../interfaces/systemConfig';
+import { baseQueryWithReauth, createRequest, CustomError } from './shared';
+
+export const systemConfigApi = createApi({
+  reducerPath: 'systemConfigApi',
+  baseQuery: baseQueryWithReauth as BaseQueryFn<string | FetchArgs, unknown, CustomError, Record<string, any>>,
+  tagTypes: ['config'],
+
+  endpoints: (builder) => ({
+    getSystemConfigs: builder.query<ConfigsResponse, void>({
+      query: () => createRequest('SystemConfig'),
+    }),
+    getSystemConfigsMenu: builder.query<ConfigMenuResponse, { id?: string }>({
+      query: ({ id }) => createRequest(`SystemConfig/${id}`),
+    }),
+  }),
+});
+
+export const { useGetSystemConfigsQuery, useGetSystemConfigsMenuQuery } = systemConfigApi;
--- src/redux/root-reducer.ts
+++ src/redux/root-reducer.ts
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
 
-import { alertApi, notificationApi, roleApi, smtpApi, templateApi, userApi } from './api';
+import { alertApi, notificationApi, roleApi, smtpApi, systemConfigApi, templateApi, userApi } from './api';
 import { databaseApi } from './api/databaseApi';
 import authReducer, { persistConfig } from './slices/auth';
 import sharedReducer from './slices/util';
@@ -15,6 +15,7 @@ export const reducers = combineReducers({
   [alertApi.reducerPath]: alertApi.reducer,
   [notificationApi.reducerPath]: notificationApi.reducer,
   [templateApi.reducerPath]: templateApi.reducer,
+  [systemConfigApi.reducerPath]: systemConfigApi.reducer,
   sharedReducer: persistReducer(persistConfig, sharedReducer),
 });
 
