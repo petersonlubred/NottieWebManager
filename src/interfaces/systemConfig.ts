0a50e360c3e55a968f33b73be98f4b6831575d6c
KEHINDE WAKEEL <kehinde.wakeel@indicina.co>
2023-03-07 07:24:33 +0100

    chore:minor update

--- src/interfaces/systemConfig.ts
+++ src/interfaces/systemConfig.ts
@@ -8,7 +8,7 @@ export interface IConfigMenu {
   configId: string;
   configValue: string;
   fieldLable: string;
-  fieldType: 'CHECKBOX' | 'TEXT' | 'NUMBER' | 'DROPDOWN' | 'TEXTAREA';
+  fieldType: 'CHECKBOX' | 'TEXT' | 'NUMBER' | 'RADIO' | 'TEXTAREA';
   lookupEndpoint: string;
 }
 export interface ConfigsResponse extends APIResponse<ISystemConfig[]> {}
