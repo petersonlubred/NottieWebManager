1b86060b61e24ff986772816322cb1411b758edf
KEHINDE WAKEEL <kehinde.wakeel@indicina.co>
2023-03-08 03:04:48 +0100

    fix: merge conflict fixed

--- src/redux/api/serviceMappingApi.ts
+++ src/redux/api/serviceMappingApi.ts
@@ -3,15 +3,39 @@ import { BaseQueryFn, createApi, FetchArgs } from '@reduxjs/toolkit/query/react'
 import { baseQueryWithReauth, createRequest, CustomError } from './shared';
 
 export const serviceMappingApi = createApi({
-    reducerPath: 'serviceMappingApi',
-    baseQuery: baseQueryWithReauth as BaseQueryFn<string | FetchArgs, unknown, CustomError, Record<string, any>>,
-    tagTypes: ['serviceMapping'],
+  reducerPath: 'serviceMappingApi',
+  baseQuery: baseQueryWithReauth as BaseQueryFn<string | FetchArgs, unknown, CustomError, Record<string, any>>,
+  tagTypes: ['service-mapping'],
 
-    endpoints: (builder) => ({
-        getServiceMapping: builder.query({
-            query: () => createRequest('ServiceMapping'),
-        }),
+  endpoints: (builder) => ({
+    getServiceMappings: builder.query<any, void>({
+      query: () => createRequest('ServiceMapping'),
     }),
+    getMapped: builder.query<any, void>({
+      query: () => createRequest('ServiceMapping/Mapped'),
+    }),
+    getMappedDetails: builder.query<any, { dataSourceId: string }>({
+      query: ({ dataSourceId }) => createRequest(`ServiceMapping/Mapped/${dataSourceId}`),
+    }),
+
+    updateMapping: builder.mutation({
+      query: (data) => {
+        return {
+          url: `ServiceMapping`,
+          method: 'post',
+          body: data,
+        };
+      },
+    }),
+    deleteMapping: builder.mutation({
+      query: ({ id }) => {
+        return {
+          url: `ServiceMapping/${id}`,
+          method: 'delete',
+        };
+      },
+    }),
+  }),
 });
 
-export const { useGetServiceMappingQuery } = serviceMappingApi;
+export const { useDeleteMappingMutation, useGetMappedDetailsQuery, useGetMappedQuery, useGetServiceMappingsQuery } = serviceMappingApi;
