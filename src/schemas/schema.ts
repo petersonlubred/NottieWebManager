169083eeee3861104a3e20d5c3bb1aa569674ffb
phenom-world <tijjken@gmail.com>
2023-03-16 23:10:14 +0100

    fix: fix bundle errors

--- src/schemas/schema.ts
+++ src/schemas/schema.ts
@@ -2,7 +2,7 @@ import * as Yup from 'yup';
 
 export const signinSchema = Yup.object({
   email: Yup.string().email('Invalid email').required('Email is required'),
-  password: Yup.string().min(8, 'Password must be at least 8 characters').required('Password is required'),
+  password: Yup.string().required('Password is required'),
 });
 
 export const userAccountSchema = Yup.object({
